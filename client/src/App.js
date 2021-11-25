047f20d0ca4637b5af85bf0cb1886800c3d3b97b
pranay joshi <prjoshi33@gmail.com>
2021-11-25 21:08:45 +0530

    screen arrangement half done

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,17 @@ import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 //import PrivateRoute from './components/PrivateRoute'
 
 //screens
-<<<<<<< HEAD
-import PrivateRoute from "./components/screens/PrivateScreen"
-import LoginScreen from "./components/screens/LoginScreen"
-import RegisterScreen from "./components/screens/RegisterScreen"
-import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen"
-import ResetPasswordScreen from "./components/screens/ResetPasswordScreen"
-import Client_Home  from './components/screens/Client/Client_Home'
-import Sys_Admin_Home from "./components/screens/System_Admin/Home"
-import TheaterAdminDashboard from "./components/screens/Theater_Admin/dashboard"
-import ScreenArrangement from './screens/TheaterAdmin/ScreenArrangement'
-=======
+
+
 import LoginScreen from "./screens/Client/LoginScreen"
 import RegisterScreen from "./screens/Client/RegisterScreen"
 import ForgotPasswordScreen from "./screens/Client/ForgotPasswordScreen"
 import ResetPasswordScreen from "./screens/Client/ResetPasswordScreen"
-import Client_Home  from './screens/Client/Client_Home'
 import Sys_Admin_Home from "./screens/System_Admin/Home"
->>>>>>> wip
+import TheaterAdminDashboard from "./screens/TheaterAdmin/dashboard"
+import ScreenArrangement from './screens/TheaterAdmin/ScreenArrangement'
+import Client_Home from './screens/Client/Client_Home'
+
 
 const App=()=>{
   return (
--- client/src/screens/TheaterAdmin/ScreenArrangement.js
+++ client/src/screens/TheaterAdmin/ScreenArrangement.js
@@ -1,45 +1,81 @@
-import { useState } from "react";
+import { ContactsTwoTone, SettingsInputAntennaTwoTone } from "@material-ui/icons";
+import { useRef, useState } from "react";
 
 function ScreenArrangement() {
-    const [row,setRow]=useState(0);
-    const [col,setCol]=useState(0);
+    const [rowstate,setRow]=useState(0);
+    const [colstate,setCol]=useState(0);
+    const [grid,setGrid]=useState([]);
 
-    const setDimensions=()=>{
-        set
+    const initialiseStatusGrid = (rows,cols)=>{
+        let statusGrid=[];
+        for(let i=0;i<rows;i++){
+            const row=[];
+            for(let j=0;j<cols;j++)
+            {
+                row.push({
+                    isSeat:true,
+                });
+            }
+            statusGrid.push(row);
+        }
+        console.log(statusGrid);
+        setGrid([...statusGrid]);
     }
+    const changeSeatStatus = (r,c,e)=>
+    {
+        grid[r][c].isSeat=!grid[r][c].isSeat;
+        setGrid([...grid])
+        console.log(grid);
+    }
+    const createRows = (i,totalCols)=>{
+            const cols=[];
+            for(let j=0;j<totalCols;j++)
+            {
+                const bg=grid[i][j].isSeat?"bg-red-600":"bg-gray-600";
+                cols.push(<div onClick={(e)=>{changeSeatStatus(i,j,e);}} className={`border ${bg} rounded-md m-1 px-2 py-1 cursor-pointer`} key={`${i}${j}`}>{j}</div>);
+            }
+            return cols;
+    }
+
     const createGrid=()=>{
-        const grid=[];
-        for(let i=0;i<rows;i++)
+        const totalRows=grid.length;
+        const totalCols=totalRows?grid[0].length:0;
+        const renderGrid=[];
+        console.log(totalRows,totalCols)
+        console.log(grid)
+        for(let i=0;i<totalRows;i++)
         {
-            grid.push(<tr>
-                <td>i</td>
-                <td>
+            renderGrid.push(<tr className="" key={i}>
+                <td className="pr-2" key={`${i}1`}>{i}</td>
+                <td className="flex" key={`${i}2`}>
                    {
-                       ()=>{
-                            const cols=[];
-                            for(let j=0;j<col;j++)
-                            {
-                                cols.push(<div>cell</div>);
-                            }
-                            return cols;
-                       } 
+                       createRows(i,totalCols).map(item=>item)
                    }
                 </td>
             </tr>)
         }
+        
+        return renderGrid;
     }
     return (
-        <div>
-            <div>
+        <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center">
+                <div>
                 <p> Row: </p>
-                <input type="numeric" onChange={(e)=>{setRow(e.target.value)}}></input>
+                <input type="numeric" className="border border-2 border-red-600 rounded-md px-2" onChange={(e)=>{setRow(e.target.value)}}></input>
+                </div>
+                <div>
                 <p> Columns: </p>
-                <input type="numeric" onChange={(e)=>{setCol(e.target.value)}}></input>
+                <input type="numeric" className="border border-2 border-red-600 rounded-md px-2" onChange={(e)=>{setCol(e.target.value)}}></input>
+                </div>
+                <button className="rounded-sm bg-red-600 p-1 mt-2" onClick={()=>{initialiseStatusGrid(rowstate,colstate); }}>Press ME!!!</button>
             </div>
-            <div>
+            <div className="mt-4">
+                <table>
                 {
-                    const grid=0
+                    createGrid().map(item=>item)
                 }
+               </table>
             </div>
 
         </div>
